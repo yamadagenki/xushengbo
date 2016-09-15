24fa842e3e536e9e40ab6b8d76312ca6bd2b7284
Shengbo.Xu <shengbo.xu@medley.jp>
2016-09-15 10:05:48 +0900

    add: blog

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "xushengbo/{year}/{month}/{day}.html"
+  blog.permalink = "posts/{year}/{month}/{day}.html"
   # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}.html"
   # blog.taglink = "tags/{tag}.html"
