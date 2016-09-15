bb09bc2f5047e000fa288695c5bc5b3d76f58204
Shengbo.Xu <shengbo.xu@medley.jp>
2016-09-15 10:06:51 +0900

    add: compiled

--- config.rb
+++ config.rb
@@ -28,7 +28,7 @@ activate :blog do |blog|
   # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
+  blog.layout = "blog"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
